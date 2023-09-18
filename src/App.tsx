85c67bde607b74e0624507d85c42cd3255f59ae7
Oleg Shilov <shilov-1@yandex.ru>
2023-09-18 22:30:00 +0300

    refactoring MenuButton;

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { About, Config, Main } from 'pages';
 
+import { MenuBtn } from 'components/MenuBtn';
+
 import './styles/styles.scss';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/config" element={<Config />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <MenuBtn />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/config" element={<Config />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,8 @@
 import { Bg } from 'components/Bg';
-import { MenuBtn } from 'components/MenuBtn';
 
 export const Main = () => {
   return (
     <>
-      <MenuBtn />
       <Bg />
     </>
   );
